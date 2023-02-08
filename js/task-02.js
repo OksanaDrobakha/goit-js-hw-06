e386cf59fb898e2d17b96b9fe3c6e3203fe74e6b
OksanaDrobakha <6249784@gmail.com>
2023-02-08 20:43:36 +0400

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const arrayIngridients = [];
+
+ingredients.forEach(function (ingredient) {
+  const addIngridients = document.createElement("li");
+  addIngridients.textContent = ingredient;
+  addIngridients.classList.add("item");
+  arrayIngridients.push(addIngridients);
+});
+
+const listIngridients = document.querySelector("#ingredients");
+
+listIngridients.append(...arrayIngridients);
