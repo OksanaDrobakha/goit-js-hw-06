0a07031da602442d4e5e32ad6c955b2372598686
OksanaDrobakha <6249784@gmail.com>
2023-02-12 00:10:13 +0400

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const backgroundBody = document.querySelector("body");
+const buttonColor = document.querySelector(".change-color");
+const saveColor = document.querySelector(".change-color");
+
+buttonColor.addEventListener("click", (event) => {
+  let color = getRandomHexColor();
+
+  backgroundBody.style.backgroundColor = color;
+
+  saveColor.textContent = color;
+});
